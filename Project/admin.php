4e4988655aba75a2ee9658f3a2973a1060630ec3
Erit18 <adrianrb111@gmail.com>
2024-10-12 22:34:50 -0500

    cambios

--- Project/admin.php
+++ Project/admin.php
@@ -166,8 +166,9 @@ if (!isset($_SESSION['usuario_id']) || $_SESSION['usuario_rol'] !== 'Administrad
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="Vista/plugin/index3.html" class="brand-link">
-                <img src="Vista/plugin/dist/img/crab128x128.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">Marcriollo</span>
+                <!-- Espacio reservado para el logo personalizado -->
+                <img src="Vista/plugin/dist/img/logo.png" alt="MindSound Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">MindSound</span>
             </a>
 
             <!-- Sidebar -->
